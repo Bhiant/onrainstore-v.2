d88645418797b38f5950b97ba7f41aa91d7e6432
Bhiant <bhiant513@gmail.com>
2022-01-27 22:08:06 +0700

    melengkapi data produk

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -66,6 +66,7 @@
                                     </a>
                                 </div>
                                 <div class="product-info">
+                                    <div class="wrap-price"><span class="product-price">{{$product->SKU}}</span></div>
                                     <a href="{{route('product.details',['slug'=>$product->slug])}}" class="product-name"><span>{{$product->name}}</span></a>
                                     <div class="wrap-price"><span class="product-price">Rp. {{$product->regular_price}}</span></div>
                                     <a href="#" class="btn add-to-cart" wire:click.prevent="store({{$product->id}},'{{$product->name}}',{{$product->regular_price}})">Add To Cart</a>
